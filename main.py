95b5ed52b98185454d59e4e9287d00807534a2d5
yaseen1998 <yasseenkouthe@eng.hu.edu>
2021-11-26 00:54:33 +0200

    add fucntion

--- main.py
+++ main.py
@@ -2,15 +2,19 @@ import cv2
 import numpy as np
 import dlib
 from filters.filter1 import filtering
-from filters.dogfilter import filteringdog
+# from filters.dogfilter import filteringdog
+from filters.mouse import filteringmouse
+
 # Loading Camera and Nose image and Creating mask
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture('baby.mp4')
+cap.set(3, 1920)
+cap.set(4, 1080)
 _, frame = cap.read()
 # print(frame)
 rows, cols, _ = frame.shape
 filter1 = np.zeros((rows, cols), np.uint8)
 while True:
-    filteringdog(cap,rows, cols)
+    filteringmouse(cap,rows, cols)
     key = cv2.waitKey(1)
     if key == 27:
         break
