1edc7bfb9feaebf683d9e7c008f0fcde2f67d04c
yaseen1998 <yasseenkouthe@eng.hu.edu>
2021-11-27 19:31:48 +0200

    add class

--- filters/video_filtering_face.py
+++ filters/video_filtering_face.py
@@ -2,11 +2,11 @@ from beardfilter import video_filtering_face
 
 # video_filtering_face("beard.png",8,1.1,1.5)
 
-# video_filtering_face("../assest/birthday-hat.png",27,1.3,1.2,yahia=True)
-# video_filtering_face("../assest/gold-crown.png",29,1.5,0.5, yahia = True)
-# video_filtering_face("../assest/queens-crown.png",29,1.5,0.5, yahia = True)
-# video_filtering_face("../assest/flower-crown.png",27,1.5,0.5,90)
+
 # video_filtering_face("../assest/sunglasses2.png",27,1,0.35) ## not working
 # video_filtering_face("../assest/funny-sunglass.png",27,1.05,0.33)
-# video_filtering_face("../assest/moustache2.png",51,1,0.5,1,-3)
-video_filtering_face("../assest/tongue.png",57,0.6,1.2,-25)
+# video_filtering_face(['../assest/moustache2.png'],[51],[1],[0.5],[1],[-3])
+video_filtering_face(["../assest/tongue.png"],[57],[0.6],[1.2],[-25],[0])
+# video_filtering_face(['../assest/hair9.png','../assest/ghoul2.png','../assest/eye1.png','../assest/eye2.png'],
+#                      [27,66,40,46],[1.5,1,0.2,0.2],[1,1,0.4,0.4],[100,20,5,5],[0,0,0,0])
+
