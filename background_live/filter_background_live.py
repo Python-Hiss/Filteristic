2c61a7891f634780396dd20a240103a93f6b0eb6
yaseen1998 <yasseenkouthe@eng.hu.edu>
2021-11-28 12:41:20 +0200

    edit

--- background_live/filter_background_live.py
+++ background_live/filter_background_live.py
@@ -1,13 +1,10 @@
 from background_live import background
 from request_image import add_path,check_image
-from os.path import exists
 
-type_filter = int(input('type_filter : '))
+type_filter = (input('add new background [yes,no] ? '))
 type_blur=int(input('number for blur start from 1 : '))
-if type_filter == 0:
-    path,img = add_path(input('path image : '))
-    background('../assest/background/back1.png', type_blur,img)
-if type_filter ==1:
-    img =check_image()
-    print(img)
-    background('../assest/background/back1.png',type_blur,img)
+if type_filter == 'yes':
+    add_path(input('path image : '))
+
+img =check_image()
+background('../assest/background/back1.png',type_blur,img)
